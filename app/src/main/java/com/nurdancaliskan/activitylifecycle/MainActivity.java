caa7decba016e5fbf3a66223c8e8273625106d1e
nurda <nurdan.caliskan26@hotmail.com>
2022-04-01 17:29:24 +0300

    First commit

---
+++ app/src/main/java/com/nurdancaliskan/activitylifecycle/MainActivity.java
@@ -0,0 +1,58 @@
+package com.nurdancaliskan.activitylifecycle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Log.d("likecycle","onRestart invoked");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        setContentView(R.layout.activity_main);
+        Log.d("lifecycle","onStart invoked");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        setContentView(R.layout.activity_main);
+        Log.d("lifecycle","onStop invoked");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        setContentView(R.layout.activity_main);
+        Log.d("lifecycle","onDestroy invoked");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        setContentView(R.layout.activity_main);
+        Log.d("lifecycle","onPause invoked");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        setContentView(R.layout.activity_main);
+        Log.d("lifecycle","onResume invoked");
+    }
+
+    @Override
+    protected void onRestart(){
+        super.onRestart();
+        setContentView(R.layout.activity_main);
+        Log.d("lifecycle","onRestart invoked");
+    }
+}
